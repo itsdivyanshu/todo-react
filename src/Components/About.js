b8fbef2bd30e96c5f61bd1c118e8aed088dcb69d
itsdivyanshu <73798194+itsdivyanshu@users.noreply.github.com>
2024-07-01 00:47:15 +0530

    Initial commit

---
+++ src/Components/About.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      This is an about component
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt natus
+        magnam culpa, explicabo vitae quidem enim harum at facere eaque in
+        perspiciatis temporibus eius qui nam aperiam porro voluptate ducimus
+        sint minus ad voluptatibus ex quis doloremque. Vel temporibus quas
+        voluptates odio iure deserunt quidem, delectus magnam quasi,
+        exercitationem possimus.
+      </p>
+    </div>
+  );
+};
+
+export default About;
